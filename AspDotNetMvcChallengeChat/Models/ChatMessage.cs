eb7bc177180612d145b4c6632a4acdac1378ce80
Juan Marin <jmarin@espol.edu.ec>
2022-06-27 01:15:43 -0500

    Entities Models added using Entity Framework

---
+++ AspDotNetMvcChallengeChat/Models/ChatMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspDotNetMvcChallengeChat.Models
+{
+    public partial class ChatMessage
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; } = null!;
+        public int ChatRoomId { get; set; }
+        public string? Message { get; set; }
+
+        public virtual ChatRoom ChatRoom { get; set; } = null!;
+        public virtual AspNetUser User { get; set; } = null!;
+    }
+}
---
+++ AspDotNetMvcChallengeChat/Models/ChatRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspDotNetMvcChallengeChat.Models
+{
+    public partial class ChatRoom
+    {
+        public ChatRoom()
+        {
+            ChatMessages = new HashSet<ChatMessage>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<ChatMessage> ChatMessages { get; set; }
+    }
+}
